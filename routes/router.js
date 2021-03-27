fe8dd693715b1737a95923aaaba35c77b5cae546
robertabononi <roberta.bononi16@gmail.com>
2021-03-27 15:31:21 -0300

    feat: home header

--- routes/router.js
+++ routes/router.js
@@ -4,9 +4,26 @@ const router = express.Router();
 
 // View Model index route (frontend)
 router.get('/', (req, res, send) => {
-    res.render('teste', {
+    res.render('home/index', {
         title: "Mochi",
-        version: "0.0.1"
+        version: "0.0.1",
+        materials: [
+            {
+                title: "Caderno universitário capa dura",
+                price: "R$ 9,90",
+                image: "https://img.kalunga.com.br/fotosdeprodutos/139100d.jpg"
+            },
+            {
+                title: "Caderno universitário capa dura 2",
+                price: "R$ 19,90",
+                image: "https://img.kalunga.com.br/fotosdeprodutos/139100d.jpg"
+            },
+            {
+                title: "Caderno universitário capa dura 3",
+                price: "R$ 29,90",
+                image: "https://img.kalunga.com.br/fotosdeprodutos/139100d.jpg"
+            }
+        ]
     });
 })
 
