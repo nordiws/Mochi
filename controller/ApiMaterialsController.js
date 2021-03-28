aa5ccd2e0feb7bd7d3e9b0ef057e2a58eca30cb4
pablo.gonzalez <pablogonzalezar@yahoo.com>
2021-03-28 15:39:12 -0300

    feat: função para devolver produtos filtrados no front

--- controller/ApiMaterialsController.js
+++ controller/ApiMaterialsController.js
@@ -1,6 +1,6 @@
 import ApiMaterialsConnection from "./ApiMaterialsConnections.js";
 
-// Importa o JSON dos estados
+// Importando os dados dos produtos
 const materialsData = async () => {
   try {
     const response = await ApiMaterialsConnection.getAllMaterials();
@@ -10,5 +10,15 @@ const materialsData = async () => {
   }
 }
 
+// Selecionando os produtos pelos codigos
+const selectedMaterialsData = async (selection) => {
+  try {
+    const response = await ApiMaterialsConnection.getAllMaterials();
+    const filteredMaterials = response.filter(material => material.Codigo.includes(selection));
+    return filteredMaterials;
+  } catch (err) {
+    console.log(err);
+  }
+}
 
-export default { materialsData };
+export default { materialsData, selectedMaterialsData };
