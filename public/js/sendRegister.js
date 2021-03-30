a823b94648abd74c15ed14db85452ec9c612a587
mirianfalconi <miriahfalconi@gmail.com>
2021-03-30 14:44:22 -0300

    fix

--- public/js/sendRegister.js
+++ public/js/sendRegister.js
@@ -1,23 +1,23 @@
 function registerForm(){
 
-  $form = [
-      { guardian_name : $("#guardian_name").val() },
-      { guardian_lastname : $("#guardian_lastname").val() },
-      { cpf : $("#cpf").val() },
-      { phone : $("#phone").val() },
-      { state : $("#state").val() },
-      { cep :  $("#cep").val() },
-      { city : $("#city").val() },
-      { street : $("#street").val() },
-      { number : $("#number").val() },
-      { school_state :  $("#school_state").val() },
-      { school_city : $("#school_city").val() },
-      { school_name : $("#school_name").val() },
-      { std_name : $("#std_name").val() },
-      { std_nickname : $("#std_nickname").val() },
-      { std_age : $("#std_age").val() },
-      { std_grade : $("#std_grade").val() }
-  ];
+  $form = {
+      guardian_name : $("#guardian_name").val() ,
+      guardian_lastname : $("#guardian_lastname").val() ,
+      cpf : $("#cpf").val(),
+      phone : $("#phone").val(),
+      state : $("#state").val(),
+      cep :  $("#cep").val(),
+      city : $("#city").val(),
+      street : $("#street").val(),
+      number : $("#number").val(),
+      school_state :  $("#school_state").val(),
+      school_city : $("#school_city").val(),
+      school_name : $("#school_name").val(),
+      std_name : $("#std_name").val(),
+      std_nickname : $("#std_nickname").val(),
+      std_age : $("#std_age").val(),
+      std_grade : $("#std_grade option:selected").val()
+  };
 
   $.ajax({
     type: "POST",
