e7527a7a62b9362bf95b53c58e92a0038dcb1fc1
SamukaWl <48766835+SamukaWenceslau@users.noreply.github.com>
2021-03-31 18:07:25 -0300

    feat: Filtrando cidade e devolvendo id

--- public/js/autocomplete.js
+++ public/js/autocomplete.js
@@ -1,26 +1,34 @@
+const elems = document.querySelector('.autocomplete');
+const searchBtn = document.getElementById("search-button");
 const options = {};
+const listCities = [];
 
 window.onload = () => {
 
     axios.get('http://localhost:3000/cities')
-    .then(response => {
-        const cities = response.data.citiesData;
-
-        // handle success
-        cities.map(element => {
-            const city_state = element.name;
-            options[city_state] = null;
-        });
-    })
-    .catch(error => {
-        // handle error
-        console.log(error);
-    })
+        .then(response => {
+            const cities = response.data.citiesData;
+            listCities.push(cities);
+            // handle success
+            cities.map(element => {
+                const city_state = element.name;
+                options[city_state] = null;
+            });
+        })
+        .catch(error => {
+            // handle error
+            console.log(error);
+        })
 }
 
 document.addEventListener('input', () => {
-    const elems = document.querySelector('.autocomplete');
-    const instances = M.Autocomplete.init(elems, { data: options, minLength: 2,limit: 5 });
+    const instances = M.Autocomplete.init(elems, { data: options, minLength: 2, limit: 5 });
 });
 
+searchBtn.addEventListener('click', () => {
 
+    const res = listCities[0].filter(item => item.name == elems.value);
+
+    //window.location.href = `http://localhost:3000/`
+    console.log(`http://localhost:3000/escolas?id=${res[0].id}`)
+})
