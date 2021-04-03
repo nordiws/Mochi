2935c5f03073a6572e620146114f8c8929e4c93f
robertabononi <roberta.bononi16@gmail.com>
2021-04-03 20:27:25 -0300

    fix: close modal comentado

--- public/js/modalEffects.js
+++ public/js/modalEffects.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 function modalEffect(str_){
   var $modal = $("#" + str_);
   var instance = M.Modal.getInstance($modal);
   // instance.close();
 }
-=======
->>>>>>> a55ba074b90666392b83728352efa27b9d1f7d44
